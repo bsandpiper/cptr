377f83fb25263aa600b19015e5cf242b46c3cf98
bsandpiper <164059498+bsandpiper@users.noreply.github.com>
2024-04-03 14:34:58 -0400

    Update tictactoefirst.py

--- tictactoefirst.py
+++ tictactoefirst.py
@@ -3,7 +3,8 @@ import turtle as t
 t.tracer(0)
 t.hideturtle()
 
-turn = "X" #initial turn????
+current_turn = "X" #initial turn????
+t.getcanvas().config(cursor="X_cursor")
 
 def line(start,end):
     t.penup()
@@ -26,106 +27,124 @@ def grid():
     line((-100, 300), (-100, -300))
     line((100, -300), (100, 300))
 
-def point_to_square(x:float, y:float):
-    if x == -250 and y == 250:
-        return 'Northwest'
-    elif x == 0 and y == 250:
-        return 'North'
-    elif x == 250 and y == 250:
-        return 'Northeast'
-    elif x == -250 and y == 0:
-        return 'West'
-    elif x == 0 and y == 0:
-        return 'Center'
-    elif x == 250 and y == 0:
-        return 'East'
-    elif x == -250 and y == -250:
-        return 'Southwest'
-    elif x == 0 and y == -250:
-        return 'South'
-    elif x == 250 and y == -250:
-        return 'Southeast'
-    else:
-        return None
 
-def square_to_point(sq: str):
-    if sq =='Northwest':
-        return (-250, 250)
-    elif sq == 'North':
-        return (0, 250)
-    elif sq == 'Northeast':
-        return (250, 250)
-    elif sq == 'West':
-        return (-250, 0)
-    elif sq == 'Center':
+def square_to_point(point_to_square): #changed this to take in the previous function as a parameter
+    if point_to_square =='Northwest':
+        return (-200, 200)
+    elif point_to_square == 'North':
+        return (0, 200)
+    elif point_to_square == 'Northeast':
+        return (200, 200)
+    elif point_to_square == 'West':
+        return (-200, 0)
+    elif point_to_square == 'Center':
         return (0, 0)
-    elif sq == 'East':
-        return (250, 0)
-    elif sq == 'Southwest':
-        return (-250, -250)
-    elif sq == 'South':
-        return (0,-250)
-    elif sq == 'Southeast':
-        return (250, -250)
+    elif point_to_square == 'East':
+        return (200, 0)
+    elif point_to_square == 'Southwest':
+        return (-200, -200)
+    elif point_to_square == 'South':
+        return (0,-200)
+    elif point_to_square == 'Southeast':
+        return (200, -200)
     else:
         return None
     
 
-def hit(x:float, y:float):
+def point_to_square(x:float, y:float):
     square = None
     if -300<x<-100 and 100<y<300:
         square = 'Northwest'
+        x,y = -200,200
     elif -100<x<100 and 100<y<300:
         square = 'North'
+        x,y = 0,200
     elif 100<x<300 and 100<y<300:
         square = 'Northeast'
+        x,y= 200,200
     elif -300<x<-100 and -100<y<100:
         square = 'West'
+        x,y = -200,0
     elif -100<x<100 and -100<y<100:
         square = 'Center'
+        x,y=0,0
     elif 100<x<300 and -100<y<100:
         square = 'East'
+        x,y=200,0
     elif -300<x<-100 and -300<y<-100:
         square = 'Southwest'
-    elif -100<x<100 and 300<y<-100:
+        x,y = -200,-200
+    elif -100<x<100 and -300<y<-100:
         square = 'South'
+        x,y = 0,-200
     elif 100<x<300 and -300<y<-100:
         square = 'Southeast'
+        x,y=200,-200
     return square
     
-
-def drawX (sq:str):
+size = 75
+def drawX(location, size): #changed the size to ex:75
+    size = 75
+    x,y = square_to_point(location)
+    print(x,y)
     t.color('blue')
     t.pensize(10)
-    x,y=square_to_point(sq)
-    line(x - 50, y- 50, x + 50, y + 50)
-    line(x- 50, y + 50, x + 50, y- 50)
+    t.shapesize(size)
+    line((x - 50, y- 50), (x + 50, y + 50))
+    line((x- 50, y + 50), (x + 50, y- 50))
 
-def drawO (sq:str):
+    t.update()
+
+radius = 50
+def drawO(location, radius):
+    x,y  = square_to_point(location)
+    print(x,y)
+    radius = 50
     t.color('red')
     t.pensize(10)
-    x,y = square_to_point(sq)
-    circle(50, x, y)
-
+    circle(radius, x, y-50)
+    t.update()
 
 def turnX():
-    if turn == "X":
-        t.getcanvas().config(cursor="X_cursor")
+    square = square_to_point()
+    size = 75
+    print(size)
+    square = square_to_point()
+    drawX(square, size)
+    t.getcanvas().config(cursor="X_cursor")
+
 
 def turnO():
-    if turn == "O":
-        t.getcanvas().config(cursor="circle")
+    square = square_to_point()
+    drawO(square, radius )
+    t.getcanvas().config(cursor="circle")
 
 def mouseclick(x:float,y:float):
-    what_square = point_to_square(x,y)
-    # if what_square is not None:
-        # draw_shape()
+    if current_turn == "X":
+        drawX(point_to_square(x,y),size)
+    elif current_turn == "O":
+        drawO(point_to_square(x,y), radius)
+    else:
+        return None
 
+#idea: functions turnX and turnO need to be in one function called changeturn, where I keep track of the turns and also keep track of the "t.onkeyrelease" function 
+def changeturn(): #refer to long comment about putting turnX and turnO into it
+    global current_turn
     
-    
-
+    if current_turn == "X":
+        t.getcanvas().config(cursor="circle")
+        current_turn ="O"
+        t.title(titlestring ="Current player: O")
+    elif current_turn == "O":
+        t.getcanvas().config(cursor="X_cursor")
+        current_turn = "X"
+        t.title(titlestring ="Current player: X")
+    print(current_turn)
 
 grid()
+t.onscreenclick(mouseclick)
+t.onkeypress(changeturn, "space")
 
+t.listen()
 t.update()
 t.mainloop()
